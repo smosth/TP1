d0736af5929845381f23875d318aa06bb1733a3c
Royce-LAYINDE <132207699+Royce-LAYINDE@users.noreply.github.com>
2023-05-06 14:04:30 +0200

    Update somme.py

--- somme.py
+++ somme.py
@@ -1,10 +1,1 @@
-#trouver la somme de deux nombres entiers
-nbr1 = input("entrer le premier nombre ")
-nbr2 = input("entrer le deuxieme nombre")
-
-# additionner les deus nombres
-s = int(nbr1) + int(nbr2)
-
-# afficher la sommen
-print("la somme de {0} et {1} est {2}".format(nbr1, nbr2, s))
-
+#Va te faire foutre toi et ton faux code que tu as ecrit
